a5f1d4e730babd41f105a51cc308d9664f67567d
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-07-24 17:42:39 -0700

    simplified Card to pass children more easily

--- sextant/src/components/UI/Card/Card.js
+++ sextant/src/components/UI/Card/Card.js
@@ -2,26 +2,17 @@
 // Source for dynamically changing dimensions of this component - haven't used yet, but might use later: https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs
 
 import React from 'react';
-// import classes from './Card.module.css';
-import Card from '@mui/material/Card';
+import classes from './Card.module.css';
+// import Card from '@mui/material/Card';
 // import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+// import CardContent from '@mui/material/CardContent';
+// import { Typography } from '@mui/material';
 // import Button from '@mui/material/Button';
 // import Typography from '@mui/material/Typography';
 
 const CardWrapper = (props) => {
   return (
-    <Card
-      sx={{
-        width: 0.8,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: 2,
-        marginBottom: 2,
-      }}
-    >
-      <CardContent>{props.children}</CardContent>
-    </Card>
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
   );
 };
 
