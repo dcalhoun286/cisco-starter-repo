2ed3529edcdba32b5bcb49771c7cc77017a18663
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-07-30 15:56:07 -0700

    url being passed as props, deleted code no longer needed

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -7,42 +7,39 @@ import CardWrapper from './components/UI/Card/Card';
 
 import FetchIPData from './components/FetchIPData/FetchIPData';
 
-const renderedIPData = (
-  <React.Fragment>
-    <CardWrapper>
-      <h3>IP Address</h3>
-      <FetchIPData />
-    </CardWrapper>
-  </React.Fragment>
-);
-
-// const DUMMY_TEST_DATA = [
-//   { id: 1, title: 'example 1 title', content: '**visualized data**' },
-//   { id: 2, title: 'example 2 title', content: '**visualized data**' },
-//   { id: 3, title: 'example 3 title', content: '**visualized data**' },
-//   { id: 4, title: 'example 4 title', content: '**visualized data**' },
-//   { id: 5, title: 'example 5 title', content: '**visualized data**' },
-// ];
-
-// const renderedDataItems = (
-//   <ul>
-//     {DUMMY_TEST_DATA.map((item) => (
-//       <li id={item.id} key={item.id}>
-//         <CardWrapper>
-//           <h3>{item.title}</h3>
-//           <hr></hr>
-//           <section>{item.content}</section>
-//         </CardWrapper>
-//       </li>
-//     ))}
-//   </ul>
-// );
 function App(props) {
+  const renderedIPv4Data = (
+    <CardWrapper>
+      <h3>IPv4 Address</h3>
+      <FetchIPData url="https://api.ipify.org?format=json" />
+    </CardWrapper>
+  );
+
+  const renderedIPv6Data = (
+    <CardWrapper>
+      <h3>IPv6 Address</h3>
+      <FetchIPData url="https://api64.ipify.org?format=json" />
+    </CardWrapper>
+  );
+
+  const ipDataList = [renderedIPv4Data, renderedIPv6Data];
+
+  let liItem = 0;
+
+  const renderedIPData = (
+    <ul>
+      {ipDataList.map((item) => (
+        <li id={liItem++} key={liItem}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+
   return (
     <div className="App">
       <Header />
       {renderedIPData}
-      {/* {renderedDataItems} */}
     </div>
   );
 }
